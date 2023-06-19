c20987d8e23a6da6ab47106eb45ef0b2ffa69869
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-06-19 23:27:13 +0300

    Calculator

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.clear();
+
+const knopkes1DOM = document.getElementById('lygu');
+
+knopkes1DOM.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('=');
+})
